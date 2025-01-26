d042013c4c342fc68676a50329334d74bb8234fb
Vishal Soni <vk1728476@gmail.com>
2025-01-26 14:43:40 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 
 // console.dir(app);
 
-let port = 3000;
+let port = 8080;
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("you contacted root path");
+});
+
+app.get("/:username/:id", (req, res) =>{
+    res.send(`welcome to the page of @${username}.`);
+    console.log(req.params)
 })
+
+
+
+
+// app.use((req, res) => {
+//     console.log("request receieved");
+//     res.send("This is a basic response");
+// })
